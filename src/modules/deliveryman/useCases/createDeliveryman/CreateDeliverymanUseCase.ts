930fed4ca9eadb46e3d018eab7be9701166c3a58
David-Gomez <davidcdepy@gmail.com>
2022-05-27 21:18:49 -0400

    added-api-test-Documentation

--- src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -17,7 +17,7 @@ export class CreateDeliverymanUseCase {
       },
     });
 
-    if (!deliverymanExist) {
+    if (deliverymanExist) {
       throw new Error("Deliveryman already exists");
     }
 
